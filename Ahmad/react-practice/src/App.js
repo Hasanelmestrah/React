cae855c467333b19b1c73c2acbac96d07aa994e3
AhmadLAhmad <ahmadd.l.ahmadd1998@gmail.com>
2022-12-20 19:06:12 +0200

    changed Name

--- Ahmad/react-practice/src/App.js
+++ Ahmad/react-practice/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -7,10 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         
-          <h2>Welcome  jnkjnkjnj</h2>
-          <h2>Welcome  jnkjnkjnj</h2>
-          <h2>Welcome  jnkjnkjnj</h2>
-          <h2>Welcome  jnkjnkjnj</h2>
+          <h2>Ahmad AL Ahmad</h2>
+          
         </div>
         
     
