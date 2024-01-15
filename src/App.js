119955a3dff169b24b6b63280f90a6a205932ddb
hasanelmestrah <hasanelmestrah@gmail.com>
2024-01-15 17:08:52 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import Forecast from './components/foreCast/Forecast';
 import Header from './components/header/Header';
 import HeroSection from './components/heroSection/HeroSection';
+import FakerWeather from "../src/components/data/FakerWeather.json"
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState()
+
+  // useEffect(async() => {
+  //   const response = fetch('http://api.openweathermap.org/data/2.5/forecast?q=London&cnt=8&units=metric&appid=57cf9da04987637a23fcbc26f5356e12'
+  //   )
+  //   const result = await response.json()
+
+  //   console.log(result);
+  // }, [])
+
+  useEffect(() => {
+    setData(FakerWeather)
+  }, [])
+
+  console.log(data);
+
   return (
     <div className="App">
       <Header />
-      <HeroSection />
+      <HeroSection listData={data} />
       <Forecast />
     </div>
   );
--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './heroSection.css'
 
-const HeroSection = () => {
+const HeroSection = (props) => {
+  
+  // const desc = props.listData.list[0].weather[0].description;
+  // const Mintemp = props.listData.list[0].main.temp_min
+  // const Maxtemp = props.listData.list[0].main.temp_min
+  // const humidity = props.listData.main.humidity;
+  // // const temprature = props.listData.main.temprature;
+  // const pressure = props.listData.main.pressure
+
+
+  // console.log(props);
+  // console.log(desc);
+  // console.log(desc, Mintemp);
+
+
+  // function kelvinToCelsius() {
+  //   return Math.round(props.data.main.temp_min - 273.15);
+  // }
+
   return (
     <div className='herosection-container'>
       <img src="https://placekitten.com/g/200/300" alt="" />
-      <h2>overcats clouds</h2>
+      {/* <h2>{desc}</h2> */}
       <div className='temprature'>
-            <h2>Temprature</h2>
-            <span>10C to 15C</span>
+        <h2>Temprature</h2>
+        {/* <span>{Mintemp} to {Maxtemp}</span> */}
       </div>
       <div className='temprature-details'>
-            <h4>Humidity</h4>
-            <span>78%</span>
-            <h4>Pressure</h4>
-            <span>1008.23</span>
+        <h4>Humidity</h4>
+        {/* <span>{props.data.main.humidity}</span> */}
+        <h4>Pressure</h4>
+        {/* <span>{props.data.main.pressure}</span> */}
       </div>
     </div>
   )
